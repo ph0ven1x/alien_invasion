d4a79d9d24ed9d3056ac72860beafec64846d250
ph0ven1x <sshengbyi@gmail.com>
2025-11-24 21:36:44 +0800

    创建子弹设置

--- settings.py
+++ settings.py
@@ -5,4 +5,11 @@ class Settings:
         self.screen_width = 1980
         self.screen_height = 1080
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 15.5
+        self.ship_speed = 1.5
+        # 子弹设置
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+
+
