2435b081d83938d29d02240fd5d396c034078fa9
ph0ven1x <sshengbyi@gmail.com>
2025-11-24 23:04:01 +0800

    修改错误

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,7 +17,7 @@ class AlienInvasion:
         # self.screen = pygame.display.set_mode(
         #     (0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
-        # self.settings.screen_height = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("AlienInvasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
