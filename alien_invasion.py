24c2754c1799b696768d74f16498f9e3bab1c9c6
ph0ven1x <sshengbyi@gmail.com>
2025-11-25 11:14:52 +0800

    添加一行外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,8 +75,16 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _create_fleet(self):
-        new_alien = Alien(self)
-        self.aliens.add(new_alien)
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += (2 * alien_width) 
+
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
@@ -89,7 +97,7 @@ class AlienInvasion:
         self.ship.blitme()
 
         self.aliens.draw(self.screen)
-              
+
         # 让最近绘制的屏幕可见
         pygame.display.flip()        
 
--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings:
     """存储游戏《外星人入侵》中所有设置的类"""
     def __init__(self):
         """初始化游戏的设置"""
-        self.screen_width = 1980
-        self.screen_height = 1080
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
         # 子弹设置
